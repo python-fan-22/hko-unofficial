a7c70462febab5767d8b7c48e823026f1250e5a8
Pro developer <135791276+python-fan-22@users.noreply.github.com>
2023-07-19 08:50:35 +0800

    added the file index
    the file index is a way for the app to keep track of all the required files on startup :D

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import utility
 from utility import take_input
+import os
+import getpass
+
+username = getpass.getuser()
 
 
 def loader():
-    startup_message = "welcome to unofficial hk weather app v1"
+    startup_message = "welcome to unofficial hk weather app v1. Please wait while the app loads."
     print(startup_message)
-    command = input_handler()
-    if command == "ctemp":
-        output = utility.current_temp()
+
+    installation_dir = r"C:Users\appdata\local\hko_unofficial"
+    with open("file_index.txt") as file:
+        file_list = [line.strip() for line in file.readlines()]
+        file_dict = {file: os.path.exists(file) for file in file_list}
+        has_settings = file_dict["settings.json"]
+        has_file_index = file_dict["file_index.txt"]
+        print(has_settings, has_file_index)
+        if has_settings and has_file_index:
+            loaded_files = True
+        elif has_settings and not has_file_index:
+            
 
 
 def input_handler():
--- utility.py
+++ utility.py
@@ -1,3 +1,33 @@
+import requests
+import json
+
+districts = [
+    'Central & Western District',
+    'Eastern District',
+    'Kwai Tsing',
+    'Islands District',
+    'North District',
+    'Sai Kung',
+    'Sha Tin',
+    'Southern District',
+    'Tai Po',
+    'Tsuen Wan',
+    'Tuen Mun',
+    'Wan Chai',
+    'Yuen Long',
+    'Yau Tsim Mong',
+    'Sham Shui Po',
+    'Kowloon City',
+    'Wong Tai Sin',
+    'Kwun Tong'
+]
+
+
+def query_district():
+    for i in districts:
+        print(i)
+
+
 def take_input():
     command = input(">")
     return command
@@ -7,5 +37,21 @@ def installer():
     print("d")
 
 
-def current_temp():
+def current_temp(district):
+    url = "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=en"
+    if district in districts:
+        request = requests.get(url)
+        weather_data = json.loads(request.text)
+        temperature_dict = []
+        for item in weather_data['rainfall']['data']:
+            temporary_dict = {'place': item['place'], 'temperature': item['max']}
+            temperature_dict.append(temporary_dict)
+            district_temp = temperature_dict[district]
+            return district_temp
+        else:
+            print("error: how did this break!?")
+    else:
+        print('error: district not found :(. try, "district-list" for a list of all valid districts')
+
+
 
